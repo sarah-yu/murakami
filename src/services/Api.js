7bffacc6e6264a656f0505496df41f87c3ac7c65
Sarah Yu <sarahyu0692@gmail.com>
2017-12-23 00:34:37 -0500

    update service paths for dev and prod

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
 export default () => {
+  let servicePath
+  if (document.location.hostname == 'localhost') {
+    servicePath = 'http://localhost:3001'
+  } else {
+    servicePath = 'https://murakami-api.herokuapp.com'
+  }
   return axios.create({
-    baseURL: `http://localhost:3001`
+    baseURL: servicePath
   })
 }
